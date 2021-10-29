0eec8e252845fa98c323020c8a0896d5cac2ac24
Melissa <melissa.scmi@gmail.com>
2021-10-29 16:13:34 -0700

    update details display

---
+++ BeastRescue/Migrations/20211029231048_ChangeSyntax.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeastRescue.Migrations
+{
+    public partial class ChangeSyntax : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 1,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "is 300+ pounds with black and pink blotch patterning & a scarred snout", "aloof, but loving" });
+
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 2,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "is young & fit with goldeny, cream-colored fur", "very sweet. She's a total goof" });
+
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 3,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "is silvery & quite fatsome", "crotchety. She's recovering from some trauma, but she's a big love-dove, really" });
+
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 4,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "looks like a haystack", "mercurial, playful, & very lazy" });
+
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 5,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "is furry, brown, & quite large", "social & snuggly" });
+
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 6,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "stinky, loud, & mustachioed", "unruly!" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 1,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "300+ pounds, black and pink blotch patterning, scarred snout", "Aloof, but loving" });
+
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 2,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "Goldeny, cream-colored fur. Slender.", "Very sweet; a goof" });
+
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 3,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "Silvery, quite fatsome", "Crotchety; recovering from trauma; big love dove, really" });
+
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 4,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "Looks like a haystack", "Mercurial, playful, lazy" });
+
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 5,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "Furry, brown, large", "Social & snuggly" });
+
+            migrationBuilder.UpdateData(
+                table: "Beasts",
+                keyColumn: "BeastId",
+                keyValue: 6,
+                columns: new[] { "Description", "Temperment" },
+                values: new object[] { "Stinky, loud, mustachioed", "Unruly!" });
+        }
+    }
+}
--- BeastRescue/Models/BeastRescueContext.cs
+++ BeastRescue/Models/BeastRescueContext.cs
@@ -13,17 +13,17 @@ namespace BeastRescue.Models
         {
             builder.Entity<Beast>()
             .HasData(
-                new Beast { BeastId = 1, Name = "Doctor", Species = "Hog", Temperment = "Aloof, but loving", Description = "300+ pounds, black and pink blotch patterning, scarred snout", Age = 15, Gender = "Male", Photo = "/../../img/pauline-bernfeld-S99OqS453go-unsplash.jpeg"},
+                new Beast { BeastId = 1, Name = "Doctor", Species = "Hog", Temperment = "aloof, but loving", Description = "is 300+ pounds with black and pink blotch patterning & a scarred snout", Age = 15, Gender = "Male", Photo = "/../../img/pauline-bernfeld-S99OqS453go-unsplash.jpeg"},
                 
-                new Beast { BeastId = 2, Name = "Murna", Species = "Cow", Temperment = "Very sweet; a goof", Description = "Goldeny, cream-colored fur. Slender.", Age = 2, Gender = "Female", Photo = "/../../img/nighthawk-shoots-BOuggN1tMEk-unsplash.jpeg"},
+                new Beast { BeastId = 2, Name = "Murna", Species = "Cow", Temperment = "very sweet. She's a total goof", Description = "is young & fit with goldeny, cream-colored fur", Age = 2, Gender = "Female", Photo = "/../../img/nighthawk-shoots-BOuggN1tMEk-unsplash.jpeg"},
 
-                new Beast { BeastId = 3, Name = "Biddy", Species = "Hog", Temperment = "Crotchety; recovering from trauma; big love dove, really", Description = "Silvery, quite fatsome", Age = 9, Gender = "Female", Photo = "/../../img/noelle-KmrmYDyiExg-unsplash.jpeg"},
+                new Beast { BeastId = 3, Name = "Biddy", Species = "Hog", Temperment = "crotchety. She's recovering from some trauma, but she's a big love-dove, really", Description = "is silvery & quite fatsome", Age = 9, Gender = "Female", Photo = "/../../img/noelle-KmrmYDyiExg-unsplash.jpeg"},
 
-                new Beast { BeastId = 4, Name = "Haystack", Species = "Hog", Temperment = "Mercurial, playful, lazy", Description = "Looks like a haystack", Age = 4, Gender = "Female", Photo = "/../../img/callum-hill-Zx6ZD3n6x4A-unsplash.jpeg"},
+                new Beast { BeastId = 4, Name = "Haystack", Species = "Hog", Temperment = "mercurial, playful, & very lazy", Description = "looks like a haystack", Age = 4, Gender = "Female", Photo = "/../../img/callum-hill-Zx6ZD3n6x4A-unsplash.jpeg"},
 
-                new Beast { BeastId = 5, Name = "Lonely", Species = "Brown Bear", Temperment = "Social & snuggly", Description = "Furry, brown, large", Age = 15, Gender = "Male", Photo = "/../../img/mark-basarab-y421kXlUOQk-unsplash.jpeg"},
+                new Beast { BeastId = 5, Name = "Lonely", Species = "Brown Bear", Temperment = "social & snuggly", Description = "is furry, brown, & quite large", Age = 15, Gender = "Male", Photo = "/../../img/mark-basarab-y421kXlUOQk-unsplash.jpeg"},
 
-                new Beast { BeastId = 6, Name = "Hobnail", Species = "Sea Lion", Temperment = "Unruly!", Description = "Stinky, loud, mustachioed", Age = 8, Gender = "Male", Photo = "/../../img/taylor-wright-xFzaWaNxXSc-unsplash.jpeg"}
+                new Beast { BeastId = 6, Name = "Hobnail", Species = "Sea Lion", Temperment = "unruly!", Description = "is stinky, loud, & mustachioed", Age = 8, Gender = "Male", Photo = "/../../img/taylor-wright-xFzaWaNxXSc-unsplash.jpeg"}
             );
         }
 
